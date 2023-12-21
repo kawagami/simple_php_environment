5fad1743636f173413f95c595df0a7a25748e306
kawagami <joelai1988@gmail.com>
2023-12-21 10:58:42 +0800

    add: key 對照較長的版本

---
+++ src/helpler.php
@@ -0,0 +1,49 @@
+<?php
+
+function searchFilesForChinese($dir, $chinese_to_code_array)
+{
+    $files = scandir($dir);
+
+    foreach ($files as $file) {
+        if ($file != '.' && $file != '..') {
+            $path = $dir . '/' . $file;
+
+            if (is_dir($path)) {
+                // 如果是目錄，遞迴搜尋
+                searchFilesForChinese($path, $chinese_to_code_array);
+            } else {
+                // 如果是檔案，讀取檔案內容
+                $content = file_get_contents($path);
+
+                // 使用正規表達式找尋兩個以上的中文字
+                preg_match_all('/[\x{4e00}-\x{9fa5}]{2,}/u', $content, $matches);
+
+                // 如果找到匹配，進行替換
+                if (!empty($matches[0])) {
+                    foreach ($matches[0] as $match) {
+                        if (isset($chinese_to_code_array[$match])) {
+                            // 取得中文對應的長度
+                            $matchLength = mb_strlen($match, 'UTF-8');
+
+                            // 檢查其他中文是否包含這個中文，且長度更長
+                            foreach ($matches[0] as $otherMatch) {
+                                $otherMatchLength = mb_strlen($otherMatch, 'UTF-8');
+                                if ($otherMatchLength > $matchLength && mb_strpos($otherMatch, $match, 0, 'UTF-8') !== false) {
+                                    // 如果找到比較長且包含的中文，不進行替換
+                                    continue 2;
+                                }
+                            }
+
+                            // 沒有更長的中文包含這個中文，進行替換
+                            $replacement = $chinese_to_code_array[$match];
+                            $content = str_replace($match, $replacement, $content);
+                        }
+                    }
+
+                    // 寫回檔案
+                    file_put_contents($path, $content);
+                }
+            }
+        }
+    }
+}
---
+++ src/main.php
@@ -0,0 +1,61 @@
+<?php
+
+require "./helpler.php";
+
+/**
+ * return [
+ *     "關閉" => '<?php echo $this->multilingual("multilingual_000001") ?>',
+ *     "錯誤訊息" => '<?php echo $this->multilingual("multilingual_000002") ?>',
+ *     "棄用" => '<?php echo $this->multilingual("multilingual_000003") ?>',
+ *     "注意" => '<?php echo $this->multilingual("multilingual_000004") ?>',
+ *     "警告" => '<?php echo $this->multilingual("multilingual_000005") ?>',
+ *     "嚴格" => '<?php echo $this->multilingual("multilingual_000006") ?>',
+ *     "同步" => '<?php echo $this->multilingual("multilingual_000007") ?>',
+ *      ......
+ */
+$chinese_to_code = "./data/chinese_to_code.php";
+
+/**
+ * return [
+ *     "multilingual_000001" => "關閉",
+ *     "multilingual_000002" => "錯誤訊息",
+ *     "multilingual_000003" => "棄用",
+ *     "multilingual_000004" => "注意",
+ *     "multilingual_000005" => "警告",
+ *     "multilingual_000006" => "嚴格",
+ *     "multilingual_000007" => "同步",
+ *     "multilingual_000008" => "失敗",
+ *     "multilingual_000009" => "非同步",
+ *      ......
+ */
+$code_to_chinese = "./data/code_to_chinese.php";
+
+// 要搜尋的目錄路徑
+$searchPath = './waiting_process';
+
+// load code_to_chinese.php
+if (is_readable($code_to_chinese)) {
+    $code_to_chinese_array = require $code_to_chinese;
+} else {
+    echo "沒讀到 code_to_chinese";
+    return;
+}
+
+// load chinese_to_code.php
+if (is_readable($chinese_to_code)) {
+    $chinese_to_code_array = require $chinese_to_code;
+} else {
+    echo "沒讀到 chinese_to_code";
+    return;
+}
+
+// 呼叫搜尋函式，傳入中文對應的陣列
+searchFilesForChinese($searchPath, $chinese_to_code_array);
+
+echo "完成中文對應替換！";
+
+// print_r($chinese_to_code_array);
+// echo count($code_to_chinese_array);
+// echo "\n";
+// echo count($chinese_to_code_array);
+// echo "\n";
